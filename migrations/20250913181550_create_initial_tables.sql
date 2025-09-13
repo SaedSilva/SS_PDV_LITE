a5d74357de7ce7bfc73f59803b1303f55763dff2
Saed Silva Sousa <saed.sousa@hotmail.com>
2025-09-13 15:42:06 -0300

    Adiciona tabelas Product, ProductSale e Sale no script de migração inicial

--- migrations/20250913181550_create_initial_tables.sql
+++ migrations/20250913181550_create_initial_tables.sql
@@ -1,9 +1,32 @@
 -- Add migration script here
 CREATE TABLE Product
 (
-    id       INTEGER PRIMARY KEY AUTOINCREMENT,
-    name     TEXT    NOT NULL,
-    price    INTEGER NOT NULL,
-    quantity INTEGER NOT NULL,
-    ean      TEXT
+    id         INTEGER PRIMARY KEY AUTOINCREMENT,
+    name       TEXT    NOT NULL,
+    price      INTEGER NOT NULL,
+    quantity   INTEGER NOT NULL,
+    ean        TEXT,
+    created_at INTEGER NOT NULL,
+    updated_at INTEGER
+);
+
+CREATE TABLE ProductSale
+(
+    id         INTEGER PRIMARY KEY AUTOINCREMENT,
+    product_id INTEGER NOT NULL,
+    sale_id    INTEGER NOT NULL,
+    quantity   INTEGER NOT NULL,
+    price      INTEGER NOT NULL,
+    created_at INTEGER NOT NULL,
+    updated_at INTEGER,
+    FOREIGN KEY (product_id) REFERENCES Product (id),
+    FOREIGN KEY (sale_id) REFERENCES Sale (id)
+);
+
+CREATE TABLE Sale
+(
+    id         INTEGER PRIMARY KEY AUTOINCREMENT,
+    total      INTEGER NOT NULL,
+    created_at INTEGER NOT NULL,
+    updated_at INTEGER
 );
